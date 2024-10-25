73befbf7e9583741057ed9dfc86f5b1ef3a45f5f
sareddy-shiva <153980370+sareddy-shiva@users.noreply.github.com>
2024-10-25 15:15:40 +0530

    third commit

---
+++ CodingLogicBuildingSeriesUsingJava/Arrays/arrays/SubArrays.java
@@ -0,0 +1,38 @@
+package arrays;
+
+import java.util.Scanner;
+
+public class SubArrays {
+
+	public static void main(String[] args) {
+		// Import and create a Scanner class
+		Scanner sc = new Scanner(System.in);
+		
+		// Taking user input n which indicates the size of the array
+		int n = sc.nextInt();
+		
+		// create an array of size n
+		int[] array = new int[n];
+		
+		// take and store the elements from the user into the array
+		for(int i = 0; i < n; i++) {
+			array[i] = sc.nextInt();
+		}
+		
+		// Forming sub arrays from array
+		for(int i = 0; i < n; i++) {
+			for(int j = i; j < n; j++) {
+				for(int k = i; k <= j; k++) {
+					System.out.print(array[k]);
+					if (k < j) { // Only print a space if not the last element
+                        System.out.print(", ");
+                    }
+				}
+				System.out.println();
+			}
+		}
+		sc.close();
+
+	}
+
+}
